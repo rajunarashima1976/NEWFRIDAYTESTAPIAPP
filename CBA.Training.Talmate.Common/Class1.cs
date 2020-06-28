6d16b2253f0dad3e0c6527f2003eb22f20519181
User6 <User6@CBAPOOL-0>
2020-06-28 15:07:39 +0000

    Empaddress3 is added

--- CBA.Training.Talmate.Common/Class1.cs
+++ CBA.Training.Talmate.Common/Class1.cs
@@ -17,5 +17,7 @@ namespace CBA.Training.Talmate.Common
         public string empaddress1 { get; set; }
 
         public string empaddress2 { get; set; }
+
+        public string empaddress3 { get; set; }
     }
 }
